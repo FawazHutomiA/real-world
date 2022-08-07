77f88718d4f60ef4ea288e034f5eb78d76ef9f30
Fawaz Hutomi A <hutomifawaza@gmail.com>
2022-08-07 07:47:02 +0700

    pagination

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,23 @@
   <div class="events">
     <h1>Events For Good</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'home', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Pervious</router-link
+      >
+      <router-link
+        id="page-next"
+        :to="{ name: 'home', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next &#62;</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -10,25 +27,39 @@
 // @ is an alias to /src
 import EventCard from "@/components/EventCard.vue";
 import EventService from "@/services/EventService";
+import { watchEffect } from "vue";
 
 export default {
   name: "HomeView",
+  props: ["page"],
   components: {
     EventCard,
   },
   data() {
     return {
       events: [],
+      totalEvents: 0,
     };
   },
   created() {
-    EventService.getEvents()
-      .then((res) => {
-        this.events = res.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    watchEffect(() => {
+      this.events = null;
+      EventService.getEvents(2, this.page)
+        .then((res) => {
+          this.events = res.data;
+          this.totalEvents = res.headers["x-total-count"];
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  },
+  computed: {
+    hasNextPage() {
+      var totalPages = Math.ceil(this.totalEvents / 2);
+
+      return this.page < totalPages;
+    },
   },
 };
 </script>
@@ -37,5 +68,23 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
